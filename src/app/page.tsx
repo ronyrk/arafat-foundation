8f7891b992b07a455978491a442a28c481528605
rakibulhasan933 <rakibulmd933@gmail.com>
2024-01-30 21:15:10 +0600

    [SETUP]

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="m-4">
+      <h1 className="text-2xl mx-4">Hello Arafat Foundation</h1>
+      <h2 className=" m-4 text-2xl text-blue-700">Coming Soon</h2>
+    </div>
+  );
+}
