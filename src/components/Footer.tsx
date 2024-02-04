8502f771df6abd8cf06f5d0202c5d823d0ad38d6
rakibulhasan933 <rakibulmd933@gmail.com>
2024-02-04 18:57:51 +0600

    [ADD] responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,9 +8,9 @@ import { Button } from "@/components/ui/button";
 
 function Footer() {
 	return (
-		<div className="md:px-20 py-4 px-4">
+		<div className="md:px-20 px-4">
 			<div className="flex md:flex-row flex-col  md:justify-between justify-center">
-				<div className="flex basis-1/4 flex-col items-center my-2">
+				<div className="flex basis-1/4 flex-col items-center my-2 bg-green-400 p-4 py-2">
 					<Image
 						alt='logo'
 						src={logo}
@@ -22,8 +22,8 @@ function Footer() {
 					<h2 className=" text-black text-[14px] py-2 px-2">আরাফাত ফাউন্ডেশন একটি অরাজনৈতিক, অলাভজনক, উম্মাহর ঐক্য , শিক্ষা , দাওয়াহ ও মানব কল্যাণে নিবে দিত সেবা মূলমূক প্রতিষ্ঠান।</h2>
 					<Button asChild className=' w-28 flex items-center bg-color-main hover:transition hover:delay-150 hover:duration-300 hover:bg-color-sub' size={"lg"}><Link href="https://arafatfoundation.org/donate/">দান করুন <span className='ml-[6px]'><HeartHandshake /></span></Link></Button>
 				</div>
-				<div className="flex basis-1/4 flex-col  px-6">
-					<h2 className="text-[24px] font-semibold border-b-4 border-black pr-4">তালিকা</h2>
+				<div className="flex basis-1/4 flex-col  px-4">
+					<h2 className="text-[24px] font-semibold border-b-4 border-black">তালিকা</h2>
 					<div className="flex flex-col py-4">
 						<Link href="/" className=' text-[15px] font-semibold flex flex-row hover:text-color-sub py-1' > <span className=''><ChevronRight /></span>হোম </Link>
 						<Link href="/" className=' text-[15px] font-semibold flex flex-row hover:text-color-sub py-1' > <span className=''><ChevronRight /></span>
@@ -38,8 +38,8 @@ function Footer() {
 							যোগাযোগ </Link>
 					</div>
 				</div>
-				<div className="flex basis-1/4 flex-col px-6">
-					<h2 className="text-[24px] font-semibold border-b-4 border-black pr-4">আমাদের কার্যক্রম</h2>
+				<div className="flex basis-1/4 flex-col px-4">
+					<h2 className="text-[24px] font-semibold border-b-4 border-black">আমাদের কার্যক্রম</h2>
 					<div className="flex flex-col py-4">
 						<Link href="/" className=' text-[15px] font-semibold flex flex-row hover:text-color-sub py-1' > <span className=''><ChevronRight /></span>
 							ইসলাম প্রচার </Link>
@@ -60,8 +60,8 @@ function Footer() {
 							ইফতার ও রমাদান ফুড বিতরণ </Link>
 					</div>
 				</div>
-				<div className="flex basis-1/4 flex-col px-6">
-					<h2 className="text-[24px] font-semibold border-b-4 border-black pr-8">যোগাযোগের তথ্য</h2>
+				<div className="flex basis-1/4 flex-col px-4">
+					<h2 className="text-[24px] font-semibold border-b-4 border-black">যোগাযোগের তথ্য</h2>
 					<div className="flex flex-col gap-0">
 						<Link href='/'>
 							<div className="flex flex-row items-center">
--- src/components/HeaderSlider.tsx
+++ src/components/HeaderSlider.tsx
@@ -8,8 +8,8 @@ function HeaderSlider() {
 		<div className='md:px-20 px-4 h-100 flex justify-start bg-color-main py-2 gap-2 items-center'>
 
 			<Image src={logo} className='mr-4 border-dashed rounded border-white pr-4 border-r-2' height={80} width={120} alt='logo' placeholder='blur' />
-			<div className="mx-[2px]">
-				<Marquee className='md:py-4 py-2  text-xl text-white'>
+			<div className="">
+				<Marquee className='md:py-4 px-4 py-2  text-xl text-white'>
 					কর্জে হাসানা; সুদ যেখানে হার মানতে বাধ্য (দরিদ্র এবং সুবিধা বঞ্চিতদের জন্য 100% সুদ-মুক্ত ঋণ ♦ কোন সুদ নেই ♦ কোন প্রসেসিং ফি নেই ♦ কোন গোপন চার্জ নেই ♦ কোন সার্ভিস চার্জ নেই ♦ কোন আবেদন ফি নেই♦ কোন লাভ নেই।)
 				</Marquee>
 			</div>
