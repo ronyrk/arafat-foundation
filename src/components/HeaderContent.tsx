118565744863e6454891ca5e7ea02e8055e6b73e
rakibulhasan933 <rakibulmd933@gmail.com>
2024-02-04 21:21:28 +0600

    [ADD] navbar and footer

--- src/components/HeaderContent.tsx
+++ src/components/HeaderContent.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeaderContent() {
 	return (
-		<div className='md:px-20 px-4 bg-white pt-8 flex items-center flex-col justify-center'>
+		<div className='md:px-20 px-4 pt-8 flex items-center flex-col justify-center'>
 			<h2 className="text-center text-xl md:px-2 px-1 text-[#333333]">বিত্তশালীরা কর্জে হাসানা নামক আর্থিক ইবাদতটি সম্পাদন করলে সমাজের অবহেলিত ও দারিদ্র্যক্লিষ্ট মানুষগুলো নিজ পায়ে দাঁড়ানোর সুযোগ পায়। আমাদের সকলের প্রচেষ্টায় অরাফাত ফাউন্ডেশনের কর্জে হাসানা প্রকল্পকে অনেক দুর এগিয়ে নিয়ে যেতে হবে। কর্জে হাসানা সময়ের ব্যবধানে সমাজ থেকে কলুষিত ও অন্ধকার দূর করে আলোকিত সমাজ উপহার দেবে । ইনশাআল্লাহ</h2>
 			<div className="border-b-4 w-20 border-color-sub pt-2 pb-4 " />
 		</div>
