e436d36ef567a51c09d202b5ee4578f758d457d9
rakibulhasan933 <rakibulmd933@gmail.com>
2024-02-04 14:13:14 +0600

    [ADD] pathname

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { AlignJustify, HeartHandshake, Linkedin, Mail, PhoneCall } from 'lucide-react';
 import Image from 'next/image';
-import Logo from '../../public/arafat-logo.png';
+import Logo from '../../public/karze-hasana1.png';
 import Link from 'next/link';
 import { Button } from "@/components/ui/button";
 import {
@@ -32,9 +32,11 @@ function Navbar() {
 				</div>
 			</div>
 			<div className="md:px-20 px-4 flex flex-row justify-between gap-2 md:gap-0 items-center">
-				<Image className='w-[120px] h-[100] object-contain py-[2px]' src={Logo} alt='logo' />
+				<Link href="/">
+					<Image className='w-[120px] h-[100] object-contain py-[2px]' src={Logo} placeholder='blur' alt='logo' />
+				</Link>
 				<div className=' hidden md:flex'>
-					<Link href='https://arafatfoundation.org' className='text-[16px] py-10 mx-3 font-semibold transition delay-300 duration-300 hover:border-color-sub hover:border-b-4 hover:text-color-sub'>
+					<Link href='/' className={`text-[16px] py-10 mx-3 font-semibold transition delay-300 duration-300 hover:border-color-sub hover:text-color-sub hover:border-b-4 ${pathname === "/" ? "border-b-4 border-color-sub text-color-sub" : ""}`}>
 						হোম
 					</Link>
 					<Link href='https://arafatfoundation.org/about-us/' className='text-[16px] py-10 mx-3 font-semibold transition delay-300 duration-300 hover:border-color-sub hover:border-b-4 hover:text-color-sub'>
@@ -43,7 +45,7 @@ function Navbar() {
 					<Link href='https://arafatfoundation.org/donation-verify/' className='text-[16px] py-10 mx-3 font-semibold transition delay-300 duration-300 hover:border-color-sub hover:text-color-sub hover:border-b-4' >
 						DONATION VERIFY
 					</Link>
-					<Link href='/karze-hasana' className={`text-[16px] py-10 mx-3 font-semibold transition delay-300 duration-300 hover:border-color-sub hover:text-color-sub hover:border-b-4 ${pathname === "/karze-hasana" ? "border-b-4 border-color-sub text-color-sub" : ""}`}>
+					<Link href='/karze-hasana' className={`text-[16px] py-10 mx-3 font-semibold transition delay-300 duration-300 hover:border-color-sub hover:text-color-sub hover:border-b-4 ${pathname === "/karze-hasana" || pathname === "/karze-hasana/branches" || pathname === "/karze-hasana/" || pathname === "/karze-hasana/donors" || pathname === "/karze-hasana/borrowers" ? "border-b-4 border-color-sub text-color-sub" : ""}`}>
 						কর্জে হাসানা
 					</Link>
 					<Link href='https://arafatfoundation.org/donation-verify/' className='text-[16px] py-10 mx-3 font-semibold transition delay-300 duration-300 hover:border-color-sub hover:text-color-sub hover:border-b-4'>
