dd1294d153818699642df6377987e89dcd2f3fd8
rakibulhasan933 <rakibulmd933@gmail.com>
2024-02-08 23:10:13 +0600

    [ADD] donor removed

--- src/app/branch/route.ts
+++ src/app/branch/route.ts
@@ -1,9 +1,7 @@
-import prisma from "@/lib/prisma"
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
 	try {
-		const result = await prisma.branch.findMany();
 		return NextResponse.json("ok");
 	} catch (error) {
 		console.log("error");
