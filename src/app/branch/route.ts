04e223f7ec2db7b4d4a80b70d63f12743007344f
rakibulhasan933 <rakibulmd933@gmail.com>
2024-02-08 23:11:22 +0600

    [ADD] api response

--- src/app/branch/route.ts
+++ src/app/branch/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export const GET = async () => {
 	try {
-		return NextResponse.json("ok");
+		return NextResponse.json({ message: "ok" });
 	} catch (error) {
 		console.log("error");
 	}
