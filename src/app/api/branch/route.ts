e553a47900683a09860bc8620c40eccafc2237ac
rakibulhasan933 <rakibulmd933@gmail.com>
2024-02-08 23:38:57 +0600

    [ADD] hello

--- src/app/api/branch/route.ts
+++ src/app/api/branch/route.ts
@@ -5,8 +5,8 @@ import { NextResponse } from "next/server";
 
 export const GET = async () => {
 	try {
-		const result = await prisma.branch.findMany();
-		return NextResponse.json("ok");
+		const result = await prisma.donor.findMany();
+		return NextResponse.json(result);
 	} catch (error) {
 		return NextResponse.json({ message: "error" });
 	}
