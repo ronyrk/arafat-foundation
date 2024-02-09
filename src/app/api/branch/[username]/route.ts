d5f73f45a3ec09548761e3e245cc215593138f06
rakibulhasan933 <rakibulmd933@gmail.com>
2024-02-09 20:27:50 +0600

    [ADD] deleted & updated

--- src/app/api/branch/[username]/route.ts
+++ src/app/api/branch/[username]/route.ts
@@ -3,11 +3,10 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export const dynamic = 'force-dynamic'
-
+// Single branch
 export const GET = async (request: Request, { params }: ParamsIProps) => {
 	try {
 		const { username } = params;
-		console.log(username);
 		const result = await prisma.branch.findUnique({
 			where: {
 				username,
@@ -18,3 +17,42 @@ export const GET = async (request: Request, { params }: ParamsIProps) => {
 		throw new Error("Server Error");
 	}
 };
+
+// Single branch Updated
+
+export const PATCH = async (request: Request, { params }: ParamsIProps) => {
+	try {
+		const { username } = params;
+		const body = await request.json();
+		const { password, psFatherName, psName, psPhone, address, mosjid } = body;
+		const result = await prisma.branch.update({
+			where: {
+				username
+			},
+			data: {
+				password,
+				psFatherName,
+				psName,
+				psPhone,
+				address,
+				mosjid
+
+			}
+		});
+		return NextResponse.json({ message: "successfully updated", result })
+	} catch (error) {
+		return NextResponse.json({ error });
+	}
+};
+
+// Deleted branch
+export const DELETE = async (request: Request, { params }: ParamsIProps) => {
+	try {
+		const { username } = params;
+		await prisma.branch.delete({ where: { username } });
+		return NextResponse.json({ message: "deleted successfully" });
+	} catch (error) {
+		return NextResponse.json({ error });
+	}
+}
+
