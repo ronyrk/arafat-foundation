1e664019306a39b1c9873e55b0b49078dc0249d4
rakibulhasan933 <rakibulmd933@gmail.com>
2024-02-11 02:18:30 +0600

    [ADD] donors and borrowers

--- src/app/karze-hasana/donors/page.tsx
+++ src/app/karze-hasana/donors/page.tsx
@@ -33,7 +33,7 @@ async function DonorList() {
 						<TableCell className="font-medium uppercase">{item.status}</TableCell>
 						<TableCell className="font-medium uppercase">
 							<Button className='bg-color-sub' size={"sm"} asChild>
-								<Link href={`/branch/${item.username}`}>DETAILS</Link>
+								<Link href={`donors/${item.username}`}>DETAILS</Link>
 							</Button>
 
 						</TableCell>
