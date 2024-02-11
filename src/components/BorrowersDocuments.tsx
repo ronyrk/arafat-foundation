5499545e673fa75d02508c9e52b8cb93756b5936
rakibulhasan933 <rakibulmd933@gmail.com>
2024-02-11 13:36:32 +0600

    [ADD] updated

--- src/components/BorrowersDocuments.tsx
+++ src/components/BorrowersDocuments.tsx
@@ -3,9 +3,6 @@ import React from 'react'
 import {
 	Dialog,
 	DialogContent,
-	DialogDescription,
-	DialogHeader,
-	DialogTitle,
 	DialogTrigger,
 } from "@/components/ui/dialog"
 import { View } from 'lucide-react';
