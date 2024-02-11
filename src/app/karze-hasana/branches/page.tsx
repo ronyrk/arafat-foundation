5debeb8721fb8c86d9b8d839ec679f8c94f9ab0a
rakibulhasan933 <rakibulmd933@gmail.com>
2024-02-12 00:35:07 +0600

    [ADD] error fix

--- src/app/karze-hasana/branches/page.tsx
+++ src/app/karze-hasana/branches/page.tsx
@@ -1,18 +1,18 @@
-// import React, { Suspense } from 'react'
-// import {
-// 	Table,
-// 	TableBody,
-// 	TableCaption,
-// 	TableCell,
-// 	TableHead,
-// 	TableHeader,
-// 	TableRow,
-// } from "@/components/ui/table";
-// import { Button } from "@/components/ui/button"
-// import Link from 'next/link';
-// import { BranchIProps } from '@/types';
-// import { unstable_noStore } from 'next/cache';
-// import { Input } from '@/components/ui/input';
+import React, { Suspense } from 'react'
+import {
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button"
+import Link from 'next/link';
+import { BranchIProps } from '@/types';
+import { unstable_noStore } from 'next/cache';
+import { Input } from '@/components/ui/input';
 
 // async function BranchList() {
 // 	unstable_noStore();
@@ -46,30 +46,30 @@
 
 
 
-// async function page() {
-// 	return (
-// 		<div className='flex flex-col'>
-// 			<div className="px-4 py-2 flex justify-end">
-// 				<Input className='w-64' type="text" placeholder="Search" />
-// 			</div>
-// 			<Table>
-// 				<TableCaption>A list of your recent invoices.</TableCaption>
-// 				<TableHeader>
-// 					<TableRow>
-// 						<TableHead>CODE</TableHead>
-// 						<TableHead className='w-[100px]'>BRANCH</TableHead>
-// 						<TableHead>DISTRICT</TableHead>
-// 						<TableHead>PS(TANA)</TableHead>
-// 						<TableHead>DETAILS</TableHead>
-// 					</TableRow>
-// 				</TableHeader>
-// 				<Suspense fallback={<h2 className=' text-center p-4'>Loading...</h2>} >
-// 					<BranchList />
-// 				</Suspense>
-// 			</Table>
+async function page() {
+	return (
+		<div className='flex flex-col'>
+			<div className="px-4 py-2 flex justify-end">
+				<Input className='w-64' type="text" placeholder="Search" />
+			</div>
+			<Table>
+				<TableCaption>A list of your recent invoices.</TableCaption>
+				<TableHeader>
+					<TableRow>
+						<TableHead>CODE</TableHead>
+						<TableHead className='w-[100px]'>BRANCH</TableHead>
+						<TableHead>DISTRICT</TableHead>
+						<TableHead>PS(TANA)</TableHead>
+						<TableHead>DETAILS</TableHead>
+					</TableRow>
+				</TableHeader>
+				<Suspense fallback={<h2 className=' text-center p-4'>Loading...</h2>} >
+					{/* <BranchList /> */}
+				</Suspense>
+			</Table>
 
-// 		</div>
-// 	)
-// }
+		</div>
+	)
+}
 
-// export default page
+export default page
