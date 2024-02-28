13539fe2e0c3ad13e026fa5aae8d2856fe22612f
rakibulhasan933 <rakibulmd933@gmail.com>
2024-02-28 21:03:13 +0600

    [ADD] development

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -20,7 +20,7 @@ export default function GlobalError({
 						</p>
 					</div>
 					<Button
-						className="inline-flex w-fit h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900"
+						className="inline-flex w-fit h-10 items-center justify-center rounded-md border border-gray-200 bg-white text-color-sub px-8 text-sm font-medium shadow-sm transition-colors hover:bg-color-sub hover:text-color-main"
 						onClick={() => reset()}
 					>
 						Refresh
