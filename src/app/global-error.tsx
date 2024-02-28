cbf1cee62255778a2222917cc4f98cb03de1c75d
rakibulhasan933 <rakibulmd933@gmail.com>
2024-02-28 20:57:29 +0600

    [ADD] development

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
-import { BugIcon, RefreshCw } from "lucide-react"
 
 export default function GlobalError({
 	error,
@@ -13,21 +12,18 @@ export default function GlobalError({
 	return (
 		<html>
 			<body>
-				<div className="flex flex-col min-h-[100vh] items-center justify-center space-y-4">
+				<div className="flex items-center justify-center min-h-[100vh] flex-col gap-2">
 					<div className="space-y-2 text-center">
-						<BugIcon className="mx-auto h-12 w-12" />
-						<div className="space-y-2">
-							<h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">Uh oh! An unexpected error occurred.</h1>
-							<p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-								Don`&rsquo;`t worry, our team has been notified. You can try refreshing the page or contact support.
-							</p>
-						</div>
+						<h1 className="text-3xl font-bold tracking-tighter sm:text-4xl lg:text-5xl">Oops! Something went wrong.</h1>
+						<p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+							We apologize for the inconvenience.
+						</p>
 					</div>
 					<Button
-						className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-color-sub"
+						className="inline-flex w-fit h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900"
 						onClick={() => reset()}
 					>
-						<RefreshCw className="w-4" />	Refresh
+						Refresh
 					</Button>
 				</div>
 			</body>
