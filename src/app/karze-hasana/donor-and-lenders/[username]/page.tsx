91b11adc26f41efd70768431fcf5d747e00078da
rakibulhasan933 <rakibulmd933@gmail.com>
2024-03-06 12:18:06 +0600

    [ADD] fontend problem fix

--- src/app/karze-hasana/donor-and-lenders/[username]/page.tsx
+++ src/app/karze-hasana/donor-and-lenders/[username]/page.tsx
@@ -92,19 +92,26 @@ async function page({ params }: ParamsIProps) {
 			return totalReturn;
 		}
 	}
+	async function getStatus(status: string) {
+		if (status === "LEADER") {
+			return "LENDER"
+		} else {
+			return status
+		}
+	};
 
 	return (
 		<div className='flex flex-col gap-3'>
 			<div className="flex md:flex-row flex-col justify-between gap-3 px-2">
 				<div className=" basis-3/12 border-[2px] p-2 flex justify-around relative rounded">
 					<PhotoBlur name={data.name} url={data.photoUrl} />
-					<span className=" absolute top-3 bg-white left-2 border-[2px] text-[13px] lowercase font-normal p-[2px] rounded">{data.status}</span>
+					<span className=" absolute top-3 bg-white left-2 border-[2px] text-[13px] lowercase font-normal p-[2px] rounded">{getStatus(data.status)}</span>
 				</div>
 				<div className="basis-9/12 border-[2px] rounded p-1 px-2 flex flex-col justify-around">
 					<h2 className=" font-semibold text-xl py-1  text-color-main">{data.name}</h2>
 					<h2 className=" font-normal text-[15px]  text-color-main"><span className="font-semibold mr-2">Lives in :</span>{data.lives} </h2>
 					<h2 className=" font-normal text-[15px]  text-color-main"><span className="font-semibold mr-2">Home town:</span>{data.hometown}</h2>
-					<h2 className=" font-normal text-[15px]  text-color-main"><span className="font-semibold mr-2">Total Lending:</span>{TotalAmount()}</h2>
+					<h2 className=" font-normal text-[15px]  text-color-main"><span className="font-semibold mr-2">{data.status === "LEADER" ? "Total Lending" : "Total Donation"} :- </span>{TotalAmount()}</h2>
 				</div>
 			</div>
 			<div className="p-4">
