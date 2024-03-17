824b427ebf41f8001dfb3a0cf19e0e882b705582
rakibulhasan933 <rakibulmd933@gmail.com>
2024-03-18 00:48:48 +0600

    [ADD] donor and leader

--- src/app/api/donor_payment/donor/[username]/route.ts
+++ src/app/api/donor_payment/donor/[username]/route.ts
@@ -7,7 +7,12 @@ export const dynamic = 'force-dynamic'
 export const GET = async (request: Request, { params }: ParamsIProps) => {
 	try {
 		const { username } = params;
-		const result = await prisma.donorPayment.findMany({ where: { donorUsername: username } });
+		const result = await prisma.donorPayment.findMany({
+			where: { donorUsername: username }, orderBy: {
+				createAt: "desc"
+			}
+		});
+		console.log(result, "data");
 		return NextResponse.json(result);
 	} catch (error) {
 		throw new Error("Server Error")
