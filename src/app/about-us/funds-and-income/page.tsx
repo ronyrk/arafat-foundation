0cfc74163f1e30eca55ab06a59b5ca43ff606ef3
rakibulhasan933 <rakibulmd933@gmail.com>
2024-03-19 16:46:39 +0600

    [ADD] about-us

--- src/app/about-us/funds-and-income/page.tsx
+++ src/app/about-us/funds-and-income/page.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function page() {
 	return (
-		<div>funds-and-income</div>
+		<div className="mx-8">
+			<h2 className=' text-2xl  font-medium'>তহবিল ও আয়ের এর উৎস
+			</h2>
+			<ul className=' list-disc my-2'>
+				<li>সমর্থক ও শুভাকাঙ্ক্ষীদের এককালীন ও নিয়মিত অনুদান।</li>
+				<li>জনসাধারণ কর্তৃক বিশেষ কোনো খাতে প্রদত্ত অনুদান।</li>
+				<li>সচ্ছল মুসলিমদের প্রদত্ত অনুদান।</li>
+			</ul>
+		</div>
 	)
 }
 
--- src/app/about-us/principles-and-ideals/page.tsx
+++ src/app/about-us/principles-and-ideals/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function page() {
 	return (
-		<div>principles-and-ideals</div>
+		<div className="px-2">
+			<h2 className=' text-2xl  font-medium'>নীতি ও আদর্শ</h2>
+		</div>
 	)
 }
 
