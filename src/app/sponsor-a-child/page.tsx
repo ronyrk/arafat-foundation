c854b93f471a5dcdc50df82f78b16963362951c1
rakibulhasan933 <rakibulmd933@gmail.com>
2024-03-30 01:05:12 +0600

    [ADD] our project

--- src/app/sponsor-a-child/page.tsx
+++ src/app/sponsor-a-child/page.tsx
@@ -14,20 +14,32 @@ async function Child() {
 	};
 	const data: ProjectsProps[] = await res.json();
 	return (
-		<div className="grid md:grid-cols-3 grid-cols-1 justify-stretch md:gap-3 gap-1 p-2">
+		<div className="grid grid-cols-2 gap-1 p-2 md:grid-cols-4 justify-stretch md:gap-3">
 			{
 				data.map((item, index) => (
-					<div key={index} className=" flex  border-2 shadow-md  justify-around flex-col">
-						<Image src={item.photoUrl} width={382} height={120} className='md:w-[382px] w-full h-[260px] object-fill rounded' alt={item.username} />
-						<div className="w-full bg-white  justify-stretch px-2">
-							<h2 className=" text-lg font-semibold text-color-main hover:text-color-sub py-2">{item.title}</h2>
-							<p className=" text-[15px] font-medium">{item.shortDes}</p>
-							<div className="flex md:flex-row  justify-between py-2">
-								<Button className=' hover:bg-color-sub' asChild>
-									<Link href={`our-projects/${item.username}`}>বিস্তারিত দেখুন</Link>
+					<div key={index} className="relative flex flex-col border-2 rounded-md shadow-md ">
+						<Image src="/hhjui.png" width={248} height={120} className='w-full h-[260px] object-fill rounded' alt={item.username} />
+						<p className="absolute px-2 py-[2px] text-sm bg-white rounded top-2 left-2">স্কুল</p>
+						<div className="w-full px-1 bg-white">
+							<h2 className="py-2 text-xl font-semibold text-color-main hover:text-color-sub">মোসাঃ মাহফুজা</h2>
+							<div className="flex flex-row py-1">
+								<h2 className="text-[15px] font-medium">স্বপ্ন :</h2>
+								<h2 className="text-[15px] pl-6 font-medium">বিসিএস ক্যাডার</h2>
+							</div>
+							<div className="flex flex-row py-1">
+								<h2 className="text-[15px]  font-medium">ফোন :</h2>
+								<h2 className="text-[15px] pl-4 font-medium">01738123456</h2>
+							</div>
+							<div className="flex flex-row py-1">
+								<h2 className="text-[15px]  inline  font-medium">ঠিকানা:</h2>
+								<h2 className="text-[15px] pl-2 font-medium">টাংগন-পশ্চিম পাড়া, ইউসুফপুর,চারঘাট, রাজশাহী</h2>
+							</div>
+							<div className="flex justify-around py-2 md:flex-row">
+								<Button className='w-[130px] px-4 text-white rounded-sm bg-color-sub hover:bg-color-main' asChild>
+									<Link href={`our-projects/${item.username}`}>প্রোফাইল দেখুন</Link>
 								</Button>
-								<Button variant={"outline"} className=' text-color-main border-color-main border-2 hover:text-white hover:border-color-sub' asChild>
-									<Link href="#">দান করুন</Link>
+								<Button className='w-[130px] px-4 text-white rounded-sm bg-color-main hover:bg-color-sub' asChild>
+									<Link href="#">স্পন্সর করুন</Link>
 								</Button>
 							</div>
 						</div>
@@ -41,13 +53,13 @@ async function Child() {
 function ChildList() {
 	return (
 		<div className='py-2'>
-			<h1 className="text-center text-4xl text-color-main font-semibold py-2">একটি শিশুর দায়িত্ব নিন</h1>
-			<h2 className="text-center text-base  text-color-main font-medium py-2">একটি শিশুর (আংশিক অথবা পূর্ণ) দায়িত্ব নেওয়ার মাধ্যমে আপনি একটি শিশুকে স্কুলে যেতে সাহায্য করতে পারেন যাতে সে ঝরে পড়ার ঝুঁকি ছাড়াই শিক্ষা লাভ করতে পারে।</h2>
-			<h2 className="text-center text-base  text-color-main  font-semibold py-2">ইসলামের দৃষ্টিতে এতিমের লালন-পালনের দায়িত্ব নেয়া জান্নাতে যাওয়ার মাধ্যম। </h2>
-			<h2 className="text-center text-base  text-color-main font-normal py-2">আল্লাহর রাসূল (ﷺ) বলেছেন, ‘যে ব্যক্তি কোনো এতিমকে আপন মাতা-পিতার সঙ্গে নিজেদের (পারিবারিক) খাবারের আয়োজনে বসায় এবং <br /> (তাকে এই পরিমাণ আহার্য দান করে যে) সে পরিতৃপ্ত হয়ে আহার করে, তাহলে তার জন্য জান্নাত ওয়াজিব হয়ে যাবে।’ <br /> (মুসনাদে আহমাদ : ১৮২৫২)</h2>
-			<h2 className="text-center text-base  text-color-main  font-semibold py-2">এতিমের অভিভাবক জান্নাতে নবীজির কাছাকাছি থাকবে </h2>
-			<div className=' flex justify-center flex-col items-center gap-2'>
-				<h1 className="text-center text-sm  text-color-main font-normal border-dotted py-2">আল্লাহর রাসূল (ﷺ) বলেছেন, ‘আমি ও এতিমের অভিভাবক জান্নাতে দুই আঙুলের ন্যায় অতি কাছাকাছি থাকব।’ (বুখারি : ৬০০৫)</h1>
+			<h1 className="py-2 text-4xl font-semibold text-center text-color-main">একটি শিশুর দায়িত্ব নিন</h1>
+			<h2 className="py-2 text-base font-medium text-center text-color-main">একটি শিশুর (আংশিক অথবা পূর্ণ) দায়িত্ব নেওয়ার মাধ্যমে আপনি একটি শিশুকে স্কুলে যেতে সাহায্য করতে পারেন যাতে সে ঝরে পড়ার ঝুঁকি ছাড়াই শিক্ষা লাভ করতে পারে।</h2>
+			<h2 className="py-2 text-base font-semibold text-center text-color-main">ইসলামের দৃষ্টিতে এতিমের লালন-পালনের দায়িত্ব নেয়া জান্নাতে যাওয়ার মাধ্যম। </h2>
+			<h2 className="py-2 text-base font-normal text-center text-color-main">আল্লাহর রাসূল (ﷺ) বলেছেন, ‘যে ব্যক্তি কোনো এতিমকে আপন মাতা-পিতার সঙ্গে নিজেদের (পারিবারিক) খাবারের আয়োজনে বসায় এবং <br /> (তাকে এই পরিমাণ আহার্য দান করে যে) সে পরিতৃপ্ত হয়ে আহার করে, তাহলে তার জন্য জান্নাত ওয়াজিব হয়ে যাবে।’ <br /> (মুসনাদে আহমাদ : ১৮২৫২)</h2>
+			<h2 className="py-2 text-base font-semibold text-center text-color-main">এতিমের অভিভাবক জান্নাতে নবীজির কাছাকাছি থাকবে </h2>
+			<div className='flex flex-col items-center justify-center gap-2 '>
+				<h1 className="py-2 text-sm font-normal text-center border-dotted text-color-main">আল্লাহর রাসূল (ﷺ) বলেছেন, ‘আমি ও এতিমের অভিভাবক জান্নাতে দুই আঙুলের ন্যায় অতি কাছাকাছি থাকব।’ (বুখারি : ৬০০৫)</h1>
 				<Image src={icon} alt='icon' />
 			</div>
 			<Suspense fallback={<h2>Loading...</h2>}>
