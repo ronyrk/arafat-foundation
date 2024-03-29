97f77cbf275c4cd5e739e14090c1d068b5bfa155
rakibulhasan933 <rakibulmd933@gmail.com>
2024-03-30 01:08:40 +0600

    [ADD] blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -15,17 +15,17 @@ async function BlogsList() {
 	};
 	const data: NewsProps[] = await res.json();
 	return (
-		<div className="grid md:grid-cols-3 grid-cols-1 justify-stretch md:gap-3 gap-1 p-2">
+		<div className="grid grid-cols-1 gap-1 p-2 md:grid-cols-3 justify-stretch md:gap-3">
 			{
 				data.map((item, index) => (
 					<Link key={index} href={`blog/${item.username}`}>
-						<div className=" flex justify-center flex-col border-2 rounded-md shadow-xl  py-2">
+						<div className="flex flex-col border-2 rounded-md shadow-xl ">
 							<Image src={item.photoUrl} width={382} height={120} className='md:w-[382px] w-full h-[260px] object-fill rounded' alt={item.username} />
-							<div className="w-full border-t-2 py-2 justify-around  bg-white px-2">
-								<h3 className=" text-sm font-medium text-color-main hover:text-color-sub py-2">Blog  /  {`${moment(item.createAt).format('DD MMMM, YYYY')}`}</h3>
-								<h2 className="  text-lg font-semibold text-color-main hover:text-color-sub py-2">{item.title}</h2>
-								<p className=" text-[14px] py-1 font-medium">{item.shortDes}</p>
-								<Link className=' text-sm font-bold py-1 hover:text-color-sub' href={`blog/${item.username}`}>CONTINUE READING</Link>
+							<div className="justify-around w-full px-2 py-2 bg-white border-t-2">
+								<h3 className="py-2 text-sm font-medium text-color-main hover:text-color-sub">Blog  /  {`${moment(item.createAt).format('DD MMMM, YYYY')}`}</h3>
+								<h2 className="py-2 text-lg font-semibold text-color-main hover:text-color-sub">{item.title}</h2>
+								<p className=" text-[14px] py-1 font-medium">{item.shortDes?.slice(0, 180)}....</p>
+								<Link className='py-1 text-sm font-bold hover:text-color-sub' href={`blog/${item.username}`}>CONTINUE READING</Link>
 							</div>
 						</div>
 					</Link>
@@ -38,8 +38,8 @@ async function BlogsList() {
 function Blogs() {
 	return (
 		<div className='py-2'>
-			<div className=' flex justify-center flex-col items-center gap-2'>
-				<h1 className="text-center text-xl  text-color-main font-semibold border-dotted py-2">ব্লগ</h1>
+			<div className='flex flex-col items-center justify-center gap-2 '>
+				<h1 className="py-2 text-xl font-semibold text-center border-dotted text-color-main">ব্লগ</h1>
 				<Image src={icon} alt='icon' />
 			</div>
 			<Suspense fallback={<h2>Loading...</h2>}>
