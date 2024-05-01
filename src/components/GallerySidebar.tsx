cdd219f60e5cdb6fabba79298b1c597b4c535a8d
MD RAKIBUL HASAN <76769537+rakibulhasan933@users.noreply.github.com>
2024-05-01 09:34:25 +0600

    Update GallerySidebar.tsx

--- src/components/GallerySidebar.tsx
+++ src/components/GallerySidebar.tsx
@@ -6,15 +6,15 @@ import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import React from 'react'
 
 
-function GallerySidebar() {
-	const { data, isLoading } = useQuery<CategoryIProps[]>({
-		queryKey: ["category"],
-		queryFn: async () => {
-			const response = await axios.get('/api/category');
-			return response.data;
-		},
-		refetchInterval: 10000,
-	});
+function GallerySidebar({ data }: { data: CategoryIProps[] }) {
+	// const { data, isLoading } = useQuery<CategoryIProps[]>({
+	// 	queryKey: ["category"],
+	// 	queryFn: async () => {
+	// 		const response = await axios.get('/api/category');
+	// 		return response.data;
+	// 	},
+	// 	refetchInterval: 10000,
+	// });
 
 	const pathname = usePathname();
 	const { replace } = useRouter();
@@ -32,20 +32,16 @@ function GallerySidebar() {
 	return (
 		<div className='flex md:flex-col flex-row flex-wrap bg-[#F1F1FA] border-2 rounded gap-2'>
 			{
-				isLoading ? <h2>Loading...</h2> : <>
-					{
-						data?.map((item, index) => (
+				data?.map((item, index) => (
 
-							<button key={index} onClick={() => {
-								handleSearch(item?.path);
-							}} className={`py-2 px-2 text-[15px] font-semibold rounded-md ${type === item.path ? "bg-color-main text-white" : " text-black  hover:bg-[#DDDCF0] hover:text-black"}`}>{item.name}</button>
-						))
-					}
-					<button onClick={() => {
-						handleSearch("video");
-					}} className={`py-2 px-2 text-[15px] font-semibold rounded-md ${type === "video" ? "bg-color-main text-white" : " text-black  hover:bg-[#DDDCF0] hover:text-black"}`}>ভিডিও</button>
-				</>
+					<button key={index} onClick={() => {
+						handleSearch(item?.path);
+					}} className={`py-2 px-2 text-[15px] font-semibold rounded-md ${type === item.path ? "bg-color-main text-white" : " text-black  hover:bg-[#DDDCF0] hover:text-black"}`}>{item.name}</button>
+				))
 			}
+			<button onClick={() => {
+				handleSearch("video");
+			}} className={`py-2 px-2 text-[15px] font-semibold rounded-md ${type === "video" ? "bg-color-main text-white" : " text-black  hover:bg-[#DDDCF0] hover:text-black"}`}>ভিডিও</button>
 		</div>
 	)
 }
