1a15acb908cdb8b8f1905d93fc90454587570adc
rakibulhasan933 <rakibulmd933@gmail.com>
2024-07-29 13:04:05 +0600

    [ADD] expenses page added

--- src/components/ExpensesDetails.tsx
+++ src/components/ExpensesDetails.tsx
@@ -76,27 +76,21 @@ function ExpensesDetails() {
     }
 
     return (
-        <div className="md:mx-20 md:my-4 my-2 text-center">
+        <div className="text-center">
             <div className="my-4 flex justify-center mx-2">
                 <h2 className=" font-medium md:text-2xl text-lg border-dashed border-2 border-indigo-600 rounded px-2 ">আমাদের আয় এবং ব্যায়ের হিসাব সমূহ</h2>
             </div>
             <div>
                 <h3 className=" text-base mb-4 text-color-main font-medium">প্রিয় “ভাই/বোন” আপনার অনুদান আমাদের কাছে আমানত। আমাদের কাছে অনুদান পাঠানোর ২৪ ঘন্টার মধ্যে নিচের লিস্ট/
-                    <Link className=' text-green-500 mx-1' href="/details-of-income">Income</Link>
+                    <Link className=' text-green-500 mx-1' href="/details-of-income-and-expenses/income">Income</Link>
                     আপনার অনুদান
                     জমা হয়েছে কি না সেটি চেক করার অনুরোধ রইলো । আপনার অনুদানটি এখানে জমা না হলে আমাদের সাথে  <Link className=' text-green-500' href="/contact-us">যোগাযোগ করুন</Link> ।
                 </h3>
-                <h3 className="mt-2  text-base text-color-main font-medium">আপনি যদি নির্দিষ্ট কোনো কাজ/প্রজেক্টের জন্য আমাদেরকে অনুদান দিয়ে থাকেন তবুও নিচের লিস্ট/<Link className=' text-green-500 mx-1' href="/details-of-expenses">Expenses</Link> থেকে নির্দিষ্ট কাজে এবং নির্দিষ্ট দিনে খরচ
+                <h3 className="mt-2  text-base text-color-main font-medium">আপনি যদি নির্দিষ্ট কোনো কাজ/প্রজেক্টের জন্য আমাদেরকে অনুদান দিয়ে থাকেন তবুও নিচের লিস্ট/<Link className=' text-green-500 mx-1' href="/details-of-income-and-expenses">Expenses</Link> থেকে নির্দিষ্ট কাজে এবং নির্দিষ্ট দিনে খরচ
                     করা হয়েছে কি না সেটা চেক করার অনুরোধ রইলো ।
                 </h3>
-                <div className="flex flex-row gap-2 justify-start mx-2">
-                    <Link href="/details-of-expenses"><h2 className={`text-2xl font-bold ${pathname === "/details-of-expenses" ? " text-color-sub" : " text-color-main"}`}>Expenses</h2></Link>
-                    <h2 className=' text-2xl text-color-main font-bold'>|</h2>
-                    <Link href="/details-of-income">
-                        <h2 className={`text-2xl font-bold ${pathname === "/details-of-income" ? " text-color-sub" : " text-color-main"}`}>Income</h2></Link>
-                </div>
             </div>
-            <div className='flex flex-col my-3 border-2 py-2 px-1 rounded-sm'>
+            <div className='flex flex-col my-3  py-2 px-1 rounded-sm'>
                 <h2 className="text-center text-2xl font-semibold text-color-main">Details of  Expenses</h2>
                 <div className="p-2 flex justify-between ">
                     <div className="grid gap-2">
