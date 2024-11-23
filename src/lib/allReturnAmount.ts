b2d7b4b1ae94fa5682f919f938b21d73fddcd139
rakibulhasan933 <rakibulmd933@gmail.com>
2024-11-23 21:39:04 +0600

    [ADD] all payment

--- src/lib/allReturnAmount.ts
+++ src/lib/allReturnAmount.ts
@@ -1,23 +1,24 @@
 "use server";
+import prisma from "./prisma";
 
 import { DonorPaymentIProps } from "@/types";
 import { unstable_noStore } from "next/cache";
 
 export const allReturnAmount = async (status: string, username: string) => {
 	unstable_noStore();
-	const response = await fetch(`https://arafatfoundation.vercel.app/api/donor_payment/donor/${username}`);
-	if (!response.ok) {
-		throw new Error("Failed fetch Data");
-	};
-	const paymentList: DonorPaymentIProps[] = await response.json();
-	if (status === "LEADER") {
-		const returnArray = paymentList.filter((item) => item.type === "return");
-		let returnStringArray: string[] = [];
-		returnArray.forEach((item) => returnStringArray.push(item.loanPayment));
-		const returnNumberArray = returnStringArray.map(Number);
-		const totalReturn = returnNumberArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 
-		return totalReturn;
+	const paymentList = await prisma.donorPayment.findMany({
+		where: {
+			donorUsername: username
+		}
+	}) as DonorPaymentIProps[];
+
+	if (status === "LEADER") {
+		let returnStringArray: string[] = [];
+		paymentList.forEach((item) => returnStringArray.push(item.loanPayment as string));
+		const returnNumberArray = returnStringArray.map(Number);
+		const total = returnNumberArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+		return `${total}`;
 	} else {
 		return 'N/A'
 	}
