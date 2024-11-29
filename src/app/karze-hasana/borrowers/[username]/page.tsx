120add03dc80d510afd2c8531d30bb375d574380
rakibulhasan933 <rakibulmd933@gmail.com>
2024-11-29 15:53:13 +0600

    [ADD] borrowers

--- src/app/karze-hasana/borrowers/[username]/page.tsx
+++ src/app/karze-hasana/borrowers/[username]/page.tsx
@@ -49,11 +49,12 @@ async function page({ params }: ParamsIProps) {
 	const { username } = params;
 
 	unstable_noStore();
-	const res = await fetch(`https://af-admin.vercel.app/api/loan_list/${username}`);
-	if (!res.ok) {
-		throw new Error("Failed to fetch data");
-	}
-	const paymentList: PaymentIProps[] = await res.json();
+
+	const paymentList = await prisma?.payment.findMany({
+		where: {
+			loanusername: username
+		}
+	}) as PaymentIProps[];
 
 	const response = await fetch(`https://arafatfoundation.vercel.app/api/loan/${username}`);
 	if (!response.ok) {
