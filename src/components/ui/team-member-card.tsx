c7b216ce5b5749c8a928a79c6b92cad0e1f3b4b6
rakibulhasan933 <rakibulmd933@gmail.com>
2025-01-05 11:02:14 +0600

    card modifed

--- src/components/ui/team-member-card.tsx
+++ src/components/ui/team-member-card.tsx
@@ -18,10 +18,10 @@ interface TeamMemberCardProps {
 
 export function TeamMemberCard({ name, type, about, phone, linkedin, facebook, photos, email, username, large = false }: TeamMemberCardProps) {
     return (
-        <div className={`relative flex flex-col items-center ${large ? 'max-w-sm mx-auto' : 'w-full'}`}>
+        <div className={`relative flex flex-col items-center w-full`}>
             <div className={`w-full aspect-square relative flex flex-col items-center justify-center rounded-lg overflow-hidden bg-gradient-to-t from-[#2D2150] from-70% to-[#FF9666] to-30% group hover:shadow-xl transition-shadow duration-300`}>
                 <div className={`absolute top-[10%] flex justify-center items-center `}>
-                    <div className={`relative ${large ? 'w-40 h-40' : 'w-32 h-32'} rounded-full overflow-hidden bg-white`}>
+                    <div className={`relative w-32 h-32 rounded-full overflow-hidden bg-white`}>
                         <Image
                             src={photos}
                             alt={name}
@@ -30,7 +30,7 @@ export function TeamMemberCard({ name, type, about, phone, linkedin, facebook, p
                         />
                     </div>
                 </div>
-                <div className="absolute bottom-[80px] flex flex-col items-center">
+                <div className="absolute top-[55%] flex flex-col items-center">
                     <h3 className="text-white font-medium text-center">{name}</h3>
                     <p className="text-gray-200 text-sm mb-2">{type}</p>
                     <Button
