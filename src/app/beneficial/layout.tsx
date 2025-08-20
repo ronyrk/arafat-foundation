3e665f1a84a8ab21870fde3af8ba3e0190a9dc5e
rakibulhasan933 <rakibulmd933@gmail.com>
2025-08-21 02:10:07 +0600

    header and conted updated

--- src/app/beneficial/layout.tsx
+++ src/app/beneficial/layout.tsx
@@ -1,6 +1,6 @@
+import BeneficialHeaderContent from "@/components/beneficial-header-content";
 import BeneficialSidebar from "@/components/beneficial-sidebar";
 import BeneficialSlider from "@/components/beneficial-slider";
-import HeaderContent from "@/components/HeaderContent";
 
 
 
@@ -14,13 +14,13 @@ export default function RootLayoutBeneficial({
     return (
         <section className="bg-[#FCFCFD]">
             <BeneficialSlider />
-            <HeaderContent />
+            <BeneficialHeaderContent />
             <div className="md:mx-20 md:my-4 my-2">
                 <div className="flex md:flex-row flex-col gap-4">
                     <div className="basis-1/5">
                         <BeneficialSidebar />
                     </div>
-                    <div className=" md:px-2 bg-white  rounded-md border-[2px] px-1 py-2 basis-4/5">{children}</div>
+                    <div className=" md:px-6 bg-white  rounded-md border-[2px] px-1 py-2 basis-4/5">{children}</div>
                 </div>
             </div>
         </section>
